249d7d1f783fa63fd85349e37d7492e963791c3b
neurohaze <boligarlarahul13@gmail.com>
2026-02-07 19:47:22 +0530

    Updated structure. Added requirements.txt and readme

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="ZERO WASTED",
+    layout="wide"
+)
+
+st.sidebar.title("ZERO WASTED")
+st.sidebar.caption("Hyperlocal resource sharing")
+
+st.write("Select a page from the sidebar")
